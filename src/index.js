ef53708d9d96b834534647b1e1837a0dbd1329d5
Rodvin Danaei <rod.danaei@gmail.com>
2021-01-22 15:44:30 -0700

    Add support for file upload via Multer

--- src/index.js
+++ src/index.js
@@ -9,6 +9,49 @@ const taskRouter = require('./routers/task')
 const app = exprees()
 const port = process.env.PORT || 3000
 
+const multer = require('multer')
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        // if(!file.originalname.endsWith('.pdf')) {
+        //     return cb(new Error('Please upload a PDF'))
+        // }
+
+        if(!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a word document'))
+        }
+
+        cb(undefined, true)
+        // cb(new Error('File must be a PDF'))
+        // cb(undefined, true)
+        // cb(undefined)
+
+    }
+})
+
+// const errorMiddleware = (req, res, next) => {
+//     throw new Error('From my middleware')
+// }
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+app.use(exprees.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+})
+
 // app.use((req, res, next) => {
 //     if (req.method === 'GET') {
 //         res.send('GET requests are disabled!')
@@ -23,16 +66,6 @@ const port = process.env.PORT || 3000
 // app.use((req, res, next) => {
 //     res.status(503).send('The sever is under maintenance, please come back shortly')
 // })
-
-app.use(exprees.json())
-app.use(userRouter)
-app.use(taskRouter)
-
-
-
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}`);
-})
 
 // const Task = require('./models/task')
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/user')
 
 const auth = require('../middleware/auth')
@@ -161,6 +163,60 @@ router.delete('/users/me', auth, async (req, res) => {
         res.send(req.user)
     } catch (error) {
         res.status(500).send()
+    }
+})
+
+const upload = multer({
+    // dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+// When there is no "dest" property setup on multer, we have access to file
+// on "req"
+router.post('/users/me/avatar', auth, upload.single('avatar') , async (req, res) => {
+    
+    // Create a const to store the output from sharp, a buffer 
+    // of modified image file which is stored into the database
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    // req.user.avatar = req.file.buffer
+    req.user.avatar = buffer
+
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        // Tell the requester whether the image is png or jpg or jpeg
+        // by using a header
+        // res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    } catch (error) {
+        res.status(400).send()
     }
 })
 // router.delete('/users/:id', auth, async (req, res) => {
